5ba8b7e73276f60f993369d02fbe047093d71ced
jacksparkelz <119563066+jacksparkelz@users.noreply.github.com>
2023-11-13 16:58:26 -0500

    Update flappy.py

--- Flappy/flappy.py
+++ Flappy/flappy.py
@@ -82,3 +82,22 @@ while True:
                       
                     # set the rate of frame per second 
                     framepersecond_clock.tick(framepersecond)
+
+                                #Checking if bird is above the sealevel
+            def isGameOver(horizontal, vertical, up_pipes, down_pipes):
+                 if vertical > elevation - 25 or vertical < 0:
+                      return True
+                 
+                 #Checking if bird hits the upper pipe or not
+                 for pipe in up_pipes:
+                      pipeHeight = game_images['pipeimage'][0].get_height()
+                      if(vertical < pipeHeight + pipe['y']
+                         and abs(horziontal - pipe['x']) < game_images['pipeimage'][0].get_width()):
+                           return True
+               # for pipe in down_pipes: 
+                #    if (vertical + game_images['flappybird'].get_height() > pipe['y'])  
+                #    and abs(horizontal - pipe['x']) < game_images['pipeimage'][0].get_width(): 
+                #        return True
+                #return False
+            
+
